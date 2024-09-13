5d855b023a5967e4a4078178d84ca05c45d99321
Marib Sultan <g.maribsultan@gmail.com>
2024-09-13 17:17:44 +0500

    groq migration complete

--- groq_terminal_ai/command_generator.py
+++ groq_terminal_ai/command_generator.py
@@ -2,7 +2,7 @@ import platform
 import os
 from groq import Groq
 
-def initialize_chat_model(model_choice):
+def initialize_chat_model():
     client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
     return client
 
@@ -10,14 +10,18 @@ def generate_command(model_choice: str, instruction: str) -> str:
     os_type = platform.system()
     shell_type = os.environ.get('SHELL') or os.environ.get('COMSPEC')
 
-    chat = initialize_chat_model(model_choice)
-    
-    system_message = (
-        "system",
-        f"You are a helpful {os_type} assistant that generates {shell_type} commands based on user instruction. You must only output the command that can be executed in {shell_type}. Do not wrap the command in quotes",
+    chat = initialize_chat_model()
+    chat_completion = chat.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are a helpful {os_type} assistant that generates {shell_type} commands based on user instruction. You must only output the command that can be executed in {shell_type}. Do not wrap the command in quotes",
+            },
+            {
+                "role": "user",
+                "content": instruction,
+            }
+        ],
+        model=model_choice,
     )
-
-    human_message = ("human", instruction)
-
-    chat_completion = chat.invoke([system_message, human_message])
     return chat_completion.choices[0].message.content
