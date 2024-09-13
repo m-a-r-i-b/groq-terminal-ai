9ab563821e66a899f46715034aca93f32f376453
Marib Sultan <g.maribsultan@gmail.com>
2024-09-14 00:51:35 +0500

    added more models

--- groq_terminal_ai/command_generator.py
+++ groq_terminal_ai/command_generator.py
@@ -11,6 +11,9 @@ def initialize_groq_client():
 def generate_command(model_choice: str, instruction: str, history_size: int = 3, use_history: bool = True) -> str:
     os_type = platform.system()
     shell_type = os.environ.get('SHELL') or os.environ.get('COMSPEC')
+    # Create a new line in the terminal for aesthetic reasons
+    # pyautogui.hotkey('enter')
+    print("Working out the command...")
 
     command_cache = load_command_cache()
     instruction_history = load_instruction_history()
@@ -18,7 +21,8 @@ def generate_command(model_choice: str, instruction: str, history_size: int = 3,
     normalized_instruction = instruction.lower().strip()
 
     if normalized_instruction in command_cache:
-        pyautogui.write(command_cache[normalized_instruction])  # Use pynput for typing
+        pyautogui.write(command_cache[normalized_instruction])
+        print("\nThere you go!")
         return
     
     client = initialize_groq_client()
@@ -61,4 +65,5 @@ def generate_command(model_choice: str, instruction: str, history_size: int = 3,
             instruction_history = instruction_history[-history_size:]
         save_instruction_history(instruction_history)
 
+    print("\nThere you go!")
     return command
--- groq_terminal_ai/models.py
+++ groq_terminal_ai/models.py
@@ -3,4 +3,9 @@ from enum import Enum
 class ModelEnum(Enum):
     LLAMA3_8B_8192 = "llama3-8b-8192"
     GEMMA2_9B_IT = "gemma2-9b-it"
+    GEMMA_7B_IT = "gemma-7b-it"
     LLAMA3_70B_8192 = "llama3-70b-8192"
+    LLAMA3_1_70B_VERSATILE = "llama-3.1-70b-versatile"
+    LLAMA3_1_8B_INSTANT = "llama-3.1-8b-instant"
+    LLAVA_V1_5_7B_4096_PREVIEW = "llava-v1.5-7b-4096-preview"
+    MIXTRAL_8X7B_32768 = "mixtral-8x7b-32768"
